d0c007d97a3ec104ab5ef6a2d74bee3777c2e356
Mateusz Grzybowski <mateusz.f.grzybowski@gmail.com>
2021-03-03 21:27:23 +0000

    Update LinearRegModel.py

--- LinearRegModel.py
+++ LinearRegModel.py
@@ -2,15 +2,15 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn import linear_model
 
-df = pd.read_csv("canada_per_capita_income.csv")
+df = pd.read_csv("A001SB1_1.csv")
+print(df)
+#plt.scatter(df["year"], df["per capita income (US$)"], color='red', marker='x')
+#plt.show()
 
-plt.scatter(df["year"], df["per capita income (US$)"], color='red', marker='x')
-plt.show()
-
-reg = linear_model.LinearRegression()
-reg.fit(df[["year"]], df["per capita income (US$)"])
+#reg = linear_model.LinearRegression()
+#reg.fit(df[["year"]], df["per capita income (US$)"])
 
 # Predicting the GDP per capita of Canada in 2020
-p = reg.predict([[2020]])
-print(p)  # Predicted to be 41,288.69 US$
+#p = reg.predict([[2020]])
+#print(p)  # Predicted to be 41,288.69 US$
 
